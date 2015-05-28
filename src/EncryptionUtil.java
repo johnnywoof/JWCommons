09405b758e1a7d0a2dd4e946367b0c539ea66730
Johnnywoof <c2972515@drdrb.com>
2015-05-28 12:58:44 -0400

    Removed the package.

--- src/EncryptionUtil.java
+++ src/EncryptionUtil.java
@@ -1,5 +1,3 @@
-package me.johnnywoof;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
--- src/TrueRandom.java
+++ src/TrueRandom.java
@@ -1,5 +1,3 @@
-package me.johnnywoof;
-
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
